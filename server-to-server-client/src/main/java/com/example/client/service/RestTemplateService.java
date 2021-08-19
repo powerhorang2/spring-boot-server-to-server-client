a8f58e6cd24ea36681e848687acded321c6680bd
powerhorang2 <tjrdbqls3@naver.com>
2021-08-19 18:14:41 +0900

    Client - RestTemplateService 클래스 누락 부분 추가

--- server-to-server-client/src/main/java/com/example/client/service/RestTemplateService.java
+++ server-to-server-client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 
 @Service
@@ -34,5 +35,35 @@ public class RestTemplateService {
 		System.out.println(result.getBody());
 		
 		return result.getBody();
+	}
+	
+	public UserResponse post() {
+		// http://localhost:9090/api/server/user/{userId}/name/{userName}
+		
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:9090")
+				.path("/api/server/user/{userId}/name/{userName}")
+				.encode()
+				.build()
+				.expand(100,"홍길동")
+				.toUri();
+		
+		System.out.println(uri);
+		
+		// http body -> object -> object mapper -> json -> rest tempalte -> http body json
+		UserRequest req = new UserRequest();
+		req.setName("김유신");
+		req.setAge(45);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+		
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getHeaders());
+		System.out.println(response.getBody());
+		
+		return response.getBody();
+		
+		
 	}
 }
